a566f9ecef60feb9a21b0cf23c744790b64ea393
edigonzales <edi.gonzales@gmail.com>
2023-02-25 20:00:38 +0100

    comments

--- lib/src/main/java/ch/so/agi/ili2meta/Ili2Meta.java
+++ lib/src/main/java/ch/so/agi/ili2meta/Ili2Meta.java
@@ -6,6 +6,13 @@ import org.slf4j.LoggerFactory;
 public class Ili2Meta {
     static Logger log = LoggerFactory.getLogger(Ili2Meta.class);
 
+    // Verschiedene Möglichkeiten?
+    // - by file and name
+    // - by repo und name
+    // - by name only?
+    // - was ist mit Versionen? Für uns spielt das keine Rolle (?).
+    
+    
     public void run() {
         log.info("Hallo Welt.");
         
